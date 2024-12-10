1d321f2a430e6e6b6194020aceca530db566fbc1
R DOONDI GANGADHAR <160998038+doondigangadhar30@users.noreply.github.com>
2024-12-11 01:09:07 +0530

    Update Right-Aligned Number Triangle.py

--- Star pattern/Right-Aligned Number Triangle.py
+++ Star pattern/Right-Aligned Number Triangle.py
@@ -1,6 +1,6 @@
-n=4
+n = int(input("Enter the number of rows: "))
 for i in range(n):
-    k=n-(i+1)
-    print(" "*k,end=" ")
-    print(""(i+1),end=" ")
+    k = n - (i + 1)
+    print(" " * k, end=" ") 
+    print("*" * (i + 1), end=" ") 
     print()
